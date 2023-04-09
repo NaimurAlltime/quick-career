e571821bb5d98dc3e747c57e46865fc0f116a69a
Naimur Rahman <naimur2231@gmail.com>
2023-04-10 00:17:13 +0600

    header section  added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,27 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/statistics">Statistics</Link>
-      <Link to="/applied_jobs">Applied Jobs</Link>
-      <Link to="/blog">Blog</Link>
+    <nav className="md:flex justify-around items-center mt-7">
+      <h5 className="text-3xl font-semibold">
+        <Link to="/">QuickCareer</Link>
+      </h5>
+      <ul className="flex flex-col md:flex-row">
+        <Link className="pr-16" to="/">
+          Home
+        </Link>
+        <Link className="pr-16" to="/statistics">
+          Statistics
+        </Link>
+        <Link className="pr-16" to="/applied_jobs">
+          Applied Jobs
+        </Link>
+        <Link className="pr-16" to="/blog">
+          Blog
+        </Link>
+      </ul>
+      <button className="bg-indigo-500 px-4 py-2 rounded-md text-lg text-white">
+        Star Applying
+      </button>
     </nav>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import Blog from "./components/Blog/Blog";
+import Home from "./components/Home/Home";
+import Main from "./components/Layout/Main";
+import Statistics from "./components/Statistics/Statistics";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>,
+      },
+      {
+        path: "/applied_jobs",
+        element: <AppliedJobs></AppliedJobs>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
